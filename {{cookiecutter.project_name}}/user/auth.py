0811a20585f6148dc6ff0f69687f9640e036a5f1
amoki <hugo@bimdata.io>
2020-08-19 13:52:44 +0200

    update User auth object

--- {{cookiecutter.project_name}}/user/auth.py
+++ {{cookiecutter.project_name}}/user/auth.py
@@ -1,4 +1,13 @@
 class User:
+    def __init__(self, id_token):
+        self.id_token = id_token
+        self.email = id_token.get("email").lower()
+        self.first_name = id_token.get("given_name")
+        self.last_name = id_token.get("family_name")
+        self.sub = id_token.get("sub")
+        self.provider = id_token.get("preferred_username").split('.')[0]
+
+
     @property
     def is_authenticated(self):
         # hack for compatibility with user permissions
@@ -6,4 +15,4 @@ class User:
 
 
 def get_user_by_id(request, id_token):
-    return User()
+    return User(id_token)
