3f22c68d4715575eb1a9ae0eed400179c8012058
amoki <hugo@bimdata.io>
2021-06-30 15:47:44 +0200

    fix webhook permissions

--- {{cookiecutter.project_name}}/webhook/views.py
+++ {{cookiecutter.project_name}}/webhook/views.py
@@ -1,12 +1,11 @@
 from rest_framework.views import APIView
 from webhook.auth import BimdataSignatureAuthentication
-from rest_framework import status, permissions
+from rest_framework import status
 from rest_framework.response import Response
 
 
 class WebHookHandler(APIView):
     authentication_classes = (BimdataSignatureAuthentication,)
-    permission_calsses = (permissions.IsAuthenticated,)
 
     def post(self, request, format=None):
         print(request.data)
